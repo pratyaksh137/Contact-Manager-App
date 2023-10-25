658bc4fa0d9c419f4cf00e3722be6f431c12bb98
pratyaksh137 <pratyaksh1032001@gmail.com>
2023-10-25 12:57:06 +0530

    server creation

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, ()=>{
+    console.log(`app is listening at ${port}`);
+});
