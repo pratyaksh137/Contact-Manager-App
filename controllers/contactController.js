2cc3ca4bdae2c1b016fdc7f12010d92281a75708
pratyaksh137 <pratyaksh1032001@gmail.com>
2023-10-27 18:00:58 +0530

    Express-async-handler

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -2,16 +2,19 @@
 //route: GET /api/contacts
 //@access: public
 
-const getContacts = (req, resp) => {
-    resp.status(200).json({ message: 'Get All Contacts' })
-};
+const asyncHandler = require('express-async-handler');
+//asyncHandler allows you to use try catch block without actually using it.
 
-const getOneContact = (req, resp) => {
+const getContacts = asyncHandler(async(req, resp) => {
+    resp.status(200).json({ message: 'Get All Contacts' })
+});
+
+const getOneContact = asyncHandler(async(req, resp) => {
     console.log(req.params); //returns in object format
     resp.status(200).json({ message: `Get contacts for id = ${req.params.id}` }) // returns only id 
-};
+});
 
-const createContact = (req, resp) => {
+const createContact = asyncHandler(async(req, resp) => {
     console.log(req.body);
     const { name, status } = req.body;
     if (!name || !status) {
@@ -19,15 +22,15 @@ const createContact = (req, resp) => {
         throw new Error('All fields are mandatory');
     }
     resp.status(200).json({ message: `Create a new contact for id = ${req.params.id}` });
-};
+});
 
-const deleteContact = (req, resp) => {
+const deleteContact = asyncHandler(async(req, resp) => {
     resp.status(200).json({ message: `Deleting contact where id = ${req.params.id}` });
-};
+});
 
-const updateContact = (req, resp) => {
+const updateContact = asyncHandler(async(req, resp) => {
     resp.status(200).json({ message: `Update Contact where id = ${req.params.id}` });
-};
+});
 
 module.exports = {
     getContacts,
